73502f781da9768c1eb078cc85f5853f22e95b6a
vikrant <vikrantyadav4802@gmail.com>
2024-07-23 14:22:49 +0530

    some issue

--- public/script.js
+++ public/script.js
@@ -29,6 +29,7 @@ function getLocation(){
 
 getLocation()
 
+var map = L.map('map').setView([0,0],13)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
